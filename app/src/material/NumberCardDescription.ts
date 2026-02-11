56d1e314b58074d6fc3b32083b9525da66698e2c
Evan Pulgino <evan.pulgino@gmail.com>
2026-02-11 12:43:54 -0800

    Add number card material

---
+++ app/src/material/NumberCardDescription.ts
@@ -0,0 +1,200 @@
+import { CardDescription } from '@gamepark/react-game'
+import { PlayerId } from '@gamepark/ipso/PlayerId'
+import { MaterialType } from '@gamepark/ipso/material/MaterialType'
+import { LocationType } from '@gamepark/ipso/material/LocationType'
+import { NumberCard } from '../../../rules/src/material/NumberCard';
+
+import numberCard1 from '../images/NumberCard1.jpg'
+import numberCard2 from '../images/NumberCard2.jpg'
+import numberCard3 from '../images/NumberCard3.jpg'
+import numberCard4 from '../images/NumberCard4.jpg'
+import numberCard5 from '../images/NumberCard5.jpg'
+import numberCard6 from '../images/NumberCard6.jpg'
+import numberCard7 from '../images/NumberCard7.jpg'
+import numberCard8 from '../images/NumberCard8.jpg'
+import numberCard9 from '../images/NumberCard9.jpg'
+import numberCard10 from '../images/NumberCard10.jpg'
+import numberCard11 from '../images/NumberCard11.jpg'
+import numberCard12 from '../images/NumberCard12.jpg'
+import numberCard13 from '../images/NumberCard13.jpg'
+import numberCard14 from '../images/NumberCard14.jpg'
+import numberCard15 from '../images/NumberCard15.jpg'
+import numberCard16 from '../images/NumberCard16.jpg'
+import numberCard17 from '../images/NumberCard17.jpg'
+import numberCard18 from '../images/NumberCard18.jpg'
+import numberCard19 from '../images/NumberCard19.jpg'
+import numberCard20 from '../images/NumberCard20.jpg'
+import numberCard21 from '../images/NumberCard21.jpg'
+import numberCard22 from '../images/NumberCard22.jpg'
+import numberCard23 from '../images/NumberCard23.jpg'
+import numberCard24 from '../images/NumberCard24.jpg'
+import numberCard25 from '../images/NumberCard25.jpg'
+import numberCard26 from '../images/NumberCard26.jpg'
+import numberCard27 from '../images/NumberCard27.jpg'
+import numberCard28 from '../images/NumberCard28.jpg'
+import numberCard29 from '../images/NumberCard29.jpg'
+import numberCard30 from '../images/NumberCard30.jpg'
+import numberCard31 from '../images/NumberCard31.jpg'
+import numberCard32 from '../images/NumberCard32.jpg'
+import numberCard33 from '../images/NumberCard33.jpg'
+import numberCard34 from '../images/NumberCard34.jpg'
+import numberCard35 from '../images/NumberCard35.jpg'
+import numberCard36 from '../images/NumberCard36.jpg'
+import numberCard37 from '../images/NumberCard37.jpg'
+import numberCard38 from '../images/NumberCard38.jpg'
+import numberCard39 from '../images/NumberCard39.jpg'
+import numberCard40 from '../images/NumberCard40.jpg'
+import numberCard41 from '../images/NumberCard41.jpg'
+import numberCard42 from '../images/NumberCard42.jpg'
+import numberCard43 from '../images/NumberCard43.jpg'
+import numberCard44 from '../images/NumberCard44.jpg'
+import numberCard45 from '../images/NumberCard45.jpg'
+import numberCard46 from '../images/NumberCard46.jpg'
+import numberCard47 from '../images/NumberCard47.jpg'
+import numberCard48 from '../images/NumberCard48.jpg'
+import numberCard49 from '../images/NumberCard49.jpg'
+import numberCard50 from '../images/NumberCard50.jpg'
+import numberCard51 from '../images/NumberCard51.jpg'
+import numberCard52 from '../images/NumberCard52.jpg'
+import numberCard53 from '../images/NumberCard53.jpg'
+import numberCard54 from '../images/NumberCard54.jpg'
+import numberCard55 from '../images/NumberCard55.jpg'
+import numberCard56 from '../images/NumberCard56.jpg'
+import numberCard57 from '../images/NumberCard57.jpg'
+import numberCard58 from '../images/NumberCard58.jpg'
+import numberCard59 from '../images/NumberCard59.jpg'
+import numberCard60 from '../images/NumberCard60.jpg'
+import numberCard61 from '../images/NumberCard61.jpg'
+import numberCard62 from '../images/NumberCard62.jpg'
+import numberCard63 from '../images/NumberCard63.jpg'
+import numberCard64 from '../images/NumberCard64.jpg'
+import numberCard65 from '../images/NumberCard65.jpg'
+import numberCard66 from '../images/NumberCard66.jpg'
+import numberCard67 from '../images/NumberCard67.jpg'
+import numberCard68 from '../images/NumberCard68.jpg'
+import numberCard69 from '../images/NumberCard69.jpg'
+import numberCard70 from '../images/NumberCard70.jpg'
+import numberCard71 from '../images/NumberCard71.jpg'
+import numberCard72 from '../images/NumberCard72.jpg'
+import numberCard73 from '../images/NumberCard73.jpg'
+import numberCard74 from '../images/NumberCard74.jpg'
+import numberCard75 from '../images/NumberCard75.jpg'
+import numberCard76 from '../images/NumberCard76.jpg'
+import numberCard77 from '../images/NumberCard77.jpg'
+import numberCard78 from '../images/NumberCard78.jpg'
+import numberCard79 from '../images/NumberCard79.jpg'
+import numberCard80 from '../images/NumberCard80.jpg'
+import numberCard81 from '../images/NumberCard81.jpg'
+import numberCard82 from '../images/NumberCard82.jpg'
+import numberCard83 from '../images/NumberCard83.jpg'
+import numberCard84 from '../images/NumberCard84.jpg'
+import numberCard85 from '../images/NumberCard85.jpg'
+import numberCard86 from '../images/NumberCard86.jpg'
+import numberCard87 from '../images/NumberCard87.jpg'
+import numberCard88 from '../images/NumberCard88.jpg'
+import numberCard89 from '../images/NumberCard89.jpg'
+import numberCard90 from '../images/NumberCard90.jpg'
+import numberCardBack from '../images/NumberCardBack.jpg'
+
+export class NumberCardDescription extends CardDescription<PlayerId, MaterialType, LocationType, NumberCard> {
+  width = 6
+  height = 6
+
+  images = {
+    [NumberCard.NumberCard1]: numberCard1,
+    [NumberCard.NumberCard2]: numberCard2,
+    [NumberCard.NumberCard3]: numberCard3,
+    [NumberCard.NumberCard4]: numberCard4,
+    [NumberCard.NumberCard5]: numberCard5,
+    [NumberCard.NumberCard6]: numberCard6,
+    [NumberCard.NumberCard7]: numberCard7,
+    [NumberCard.NumberCard8]: numberCard8,
+    [NumberCard.NumberCard9]: numberCard9,
+    [NumberCard.NumberCard10]: numberCard10,
+    [NumberCard.NumberCard11]: numberCard11,
+    [NumberCard.NumberCard12]: numberCard12,
+    [NumberCard.NumberCard13]: numberCard13,
+    [NumberCard.NumberCard14]: numberCard14,
+    [NumberCard.NumberCard15]: numberCard15,
+    [NumberCard.NumberCard16]: numberCard16,
+    [NumberCard.NumberCard17]: numberCard17,
+    [NumberCard.NumberCard18]: numberCard18,
+    [NumberCard.NumberCard19]: numberCard19,
+    [NumberCard.NumberCard20]: numberCard20,
+    [NumberCard.NumberCard21]: numberCard21,
+    [NumberCard.NumberCard22]: numberCard22,
+    [NumberCard.NumberCard23]: numberCard23,
+    [NumberCard.NumberCard24]: numberCard24,
+    [NumberCard.NumberCard25]: numberCard25,
+    [NumberCard.NumberCard26]: numberCard26,
+    [NumberCard.NumberCard27]: numberCard27,
+    [NumberCard.NumberCard28]: numberCard28,
+    [NumberCard.NumberCard29]: numberCard29,
+    [NumberCard.NumberCard30]: numberCard30,
+    [NumberCard.NumberCard31]: numberCard31,
+    [NumberCard.NumberCard32]: numberCard32,
+    [NumberCard.NumberCard33]: numberCard33,
+    [NumberCard.NumberCard34]: numberCard34,
+    [NumberCard.NumberCard35]: numberCard35,
+    [NumberCard.NumberCard36]: numberCard36,
+    [NumberCard.NumberCard37]: numberCard37,
+    [NumberCard.NumberCard38]: numberCard38,
+    [NumberCard.NumberCard39]: numberCard39,
+    [NumberCard.NumberCard40]: numberCard40,
+    [NumberCard.NumberCard41]: numberCard41,
+    [NumberCard.NumberCard42]: numberCard42,
+    [NumberCard.NumberCard43]: numberCard43,
+    [NumberCard.NumberCard44]: numberCard44,
+    [NumberCard.NumberCard45]: numberCard45,
+    [NumberCard.NumberCard46]: numberCard46,
+    [NumberCard.NumberCard47]: numberCard47,
+    [NumberCard.NumberCard48]: numberCard48,
+    [NumberCard.NumberCard49]: numberCard49,
+    [NumberCard.NumberCard50]: numberCard50,
+    [NumberCard.NumberCard51]: numberCard51,
+    [NumberCard.NumberCard52]: numberCard52,
+    [NumberCard.NumberCard53]: numberCard53,
+    [NumberCard.NumberCard54]: numberCard54,
+    [NumberCard.NumberCard55]: numberCard55,
+    [NumberCard.NumberCard56]: numberCard56,
+    [NumberCard.NumberCard57]: numberCard57,
+    [NumberCard.NumberCard58]: numberCard58,
+    [NumberCard.NumberCard59]: numberCard59,
+    [NumberCard.NumberCard60]: numberCard60,
+    [NumberCard.NumberCard61]: numberCard61,
+    [NumberCard.NumberCard62]: numberCard62,
+    [NumberCard.NumberCard63]: numberCard63,
+    [NumberCard.NumberCard64]: numberCard64,
+    [NumberCard.NumberCard65]: numberCard65,
+    [NumberCard.NumberCard66]: numberCard66,
+    [NumberCard.NumberCard67]: numberCard67,
+    [NumberCard.NumberCard68]: numberCard68,
+    [NumberCard.NumberCard69]: numberCard69,
+    [NumberCard.NumberCard70]: numberCard70,
+    [NumberCard.NumberCard71]: numberCard71,
+    [NumberCard.NumberCard72]: numberCard72,
+    [NumberCard.NumberCard73]: numberCard73,
+    [NumberCard.NumberCard74]: numberCard74,
+    [NumberCard.NumberCard75]: numberCard75,
+    [NumberCard.NumberCard76]: numberCard76,
+    [NumberCard.NumberCard77]: numberCard77,
+    [NumberCard.NumberCard78]: numberCard78,
+    [NumberCard.NumberCard79]: numberCard79,
+    [NumberCard.NumberCard80]: numberCard80,
+    [NumberCard.NumberCard81]: numberCard81,
+    [NumberCard.NumberCard82]: numberCard82,
+    [NumberCard.NumberCard83]: numberCard83,
+    [NumberCard.NumberCard84]: numberCard84,
+    [NumberCard.NumberCard85]: numberCard85,
+    [NumberCard.NumberCard86]: numberCard86,
+    [NumberCard.NumberCard87]: numberCard87,
+    [NumberCard.NumberCard88]: numberCard88,
+    [NumberCard.NumberCard89]: numberCard89,
+    [NumberCard.NumberCard90]: numberCard90,
+  }
+
+  backImage = numberCardBack
+
+}
+
+export const numberCardDescription = new NumberCardDescription()
